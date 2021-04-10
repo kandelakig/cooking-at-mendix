921658310b67e150f3d1c1149b5e813bb7391ea0
Giorgi Kandelaki <kandelakig@gmail.com>
2021-04-10 16:12:42 +0400

    models

---
+++ src/main/java/com/kandela/mendix/cooking/model/Recipe.java
@@ -0,0 +1,98 @@
+package com.kandela.mendix.cooking.model;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+public class Recipe {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @Column(nullable = false, unique = true)
+  private String title;
+
+  @ManyToMany
+  private Set<Category> categories;
+
+  private Integer yield;
+
+  @OneToMany(cascade = CascadeType.ALL)
+  private Set<RecipeIngredient> ingredients;
+
+  @ElementCollection
+  private Set<String> steps;
+
+  public Recipe() {
+    super();
+  }
+
+  public Recipe(String title, Set<Category> categories, Integer yield, Set<RecipeIngredient> ingredients, Set<String> steps) {
+    super();
+    this.title = title;
+    this.categories = categories;
+    this.yield = yield;
+    this.ingredients = ingredients;
+    this.steps = steps;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public Set<Category> getCategories() {
+    return categories;
+  }
+
+  public void setCategories(Set<Category> categories) {
+    this.categories = categories;
+  }
+
+  public Integer getYield() {
+    return yield;
+  }
+
+  public void setYield(Integer yield) {
+    this.yield = yield;
+  }
+
+  public Set<RecipeIngredient> getIngredients() {
+    return ingredients;
+  }
+
+  public void setIngredients(Set<RecipeIngredient> ingredients) {
+    this.ingredients = ingredients;
+  }
+
+  public Set<String> getSteps() {
+    return steps;
+  }
+
+  public void setSteps(Set<String> steps) {
+    this.steps = steps;
+  }
+
+}
---
+++ src/main/java/com/kandela/mendix/cooking/model/RecipeIngredient.java
@@ -0,0 +1,66 @@
+package com.kandela.mendix.cooking.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class RecipeIngredient {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  private String item;
+
+  private String quantity;
+
+  private String unit;
+
+  public RecipeIngredient() {
+    super();
+  }
+
+  public RecipeIngredient(String item, String quantity, String unit) {
+    super();
+    this.item = item;
+    this.quantity = quantity;
+    this.unit = unit;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getItem() {
+    return item;
+  }
+
+  public void setItem(String item) {
+    this.item = item;
+  }
+
+  public String getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(String quantity) {
+    this.quantity = quantity;
+  }
+
+  public String getUnit() {
+    return unit;
+  }
+
+  public void setUnit(String unit) {
+    this.unit = unit;
+  }
+
+}
