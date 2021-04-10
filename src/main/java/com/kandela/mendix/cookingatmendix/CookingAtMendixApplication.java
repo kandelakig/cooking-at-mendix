cd00e9cb3d53bb1abf7a6a6fdddad9ae2b67b5c5
Giorgi Kandelaki <kandelakig@gmail.com>
2021-04-10 15:26:35 +0400

    initialized Spring-Boot-Project

---
+++ src/main/java/com/kandela/mendix/cookingatmendix/CookingAtMendixApplication.java
@@ -0,0 +1,13 @@
+package com.kandela.mendix.cookingatmendix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingAtMendixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookingAtMendixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kandela/mendix/cookingatmendix/CookingAtMendixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kandela.mendix.cookingatmendix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingAtMendixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
